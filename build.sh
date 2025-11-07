027f409964e88c67fad0380cbfac6096f0d4d836
winalize <sinasikant2012@gmail.com>
2025-11-07 18:56:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Sistem güncellemesi (Render izin verdiği kadar)
-sudo apt-get update -y || true
+# Render ortamında sudo kullanılamaz, o yüzden Java'yı manuel olarak indiriyoruz
+echo "Downloading and setting up OpenJDK 17..."
+mkdir -p /tmp/java
+curl -L -o /tmp/java/openjdk.tar.gz https://download.java.net/java/GA/jdk17/0d483a2b78e04e6c9ad9a8e9f3b82a58/35/GPL/openjdk-17_linux-x64_bin.tar.gz
+tar -xzf /tmp/java/openjdk.tar.gz -C /tmp/java
+export JAVA_HOME=/tmp/java/jdk-17
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
 
-# Java JDK kurulumu (Android derleme için gerekli)
-sudo apt-get install -y openjdk-17-jdk || true
-
-# Python ortamı ve araçlar
+# Python ortamını hazırlıyoruz
 pip install --upgrade pip setuptools wheel
-
-# Buildozer ve bağımlılıkları
 pip install buildozer cython virtualenv
 
-# distutils modülü artık setuptools içinde, o yüzden ekstra gerek yok
-# Ancak pip cache tazelemek için:
-pip install --upgrade setuptools
-
-# Derleme
+# Derleme işlemi
 buildozer -v android debug
