33c61e89d7a7359e2431a9793186828858b3d103
winalize <sinasikant2012@gmail.com>
2025-11-07 19:05:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Python ortamını hazırla
+# Ortam hazırlığı
 pip install --upgrade pip setuptools wheel
 pip install buildozer cython virtualenv
 
-# Java JDK 17 kurulumu (Adoptium)
+# Java 17 kurulumu
 echo "Installing OpenJDK 17..."
 mkdir -p /tmp/jdk
 curl -L -o /tmp/jdk/openjdk17.tar.gz https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11%2B9/OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz
@@ -13,8 +13,9 @@ tar -xzf /tmp/jdk/openjdk17.tar.gz -C /tmp/jdk
 export JAVA_HOME=$(find /tmp/jdk -type d -name "jdk-17*" | head -n 1)
 export PATH=$JAVA_HOME/bin:$PATH
 
-echo "Java version check:"
-java -version || echo "Java not found!"
-
-# Derleme başlat
-buildozer -v android debug
+# Android SDK kurulumu
+echo "Downloading Android SDK..."
+mkdir -p ~/.android
+mkdir -p /tmp/android
+curl -L -o /tmp/android/cmdline-tools.zip https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip
+unzip -q /tmp/android/cmdline-tools.zip -d /tmp/android
